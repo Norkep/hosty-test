477ad7b31d3f94d6d4ede1fc54c0ea9616790bb2
Norkep <jallaismael@gmail.com>
2017-05-13 14:24:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo wget -c https://github.com/astrolince/hosty/raw/master/hosty -O /usr/local/bin/hosty && sudo chmod +x /usr/local/bin/hosty
+sudo wget -c https://raw.githubusercontent.com/Norkep/hosty-test/master/hosty -O /usr/local/bin/hosty && sudo chmod +x /usr/local/bin/hosty
